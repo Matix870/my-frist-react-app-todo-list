f029fbd3a6203b4f915486fa8867171deff8489d
Matix870 <mateuszmaslach96@gmail.com>
2026-02-22 18:51:16 +0100

    Update TaskList.jsx

--- TaskList.jsx
+++ TaskList.jsx
@@ -1,14 +1,30 @@
 import TaskItem from "./TaskItem";
 
-const TaskList = ({ tasks }) => {
+function TaskList({ tasks, onDeleteTask, onToggleTask }) {
   return (
-    <div>
-      {tasks.map((task) => (
-        <TaskItem 
-          key={task.id} 
-          task={task}
-        />
-      ))}
+    <div
+      className="mt-4 rounded-lg p-4 bg-white transition-colors duration-300 text-black"
+      style={{
+        maxHeight: '350px',
+        overflowY: 'auto'
+      }}
+    >
+      {tasks.length === 0 ? (
+        <h3 className="text-center text-black py-4 text-xl">
+          Brak zadań. Dodaj coś! 
+        </h3>
+      ) : (
+        <ul className="space-y-2">
+          {tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              onDelete={onDeleteTask}
+              onToggle={onToggleTask}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
