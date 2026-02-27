2b5346c7ec44f7a53c468ce218035e4444b9c79f
Matix870 <mateuszmaslach96@gmail.com>
2026-02-27 16:38:08 +0100

    Update TaskList.jsx

--- TaskList.jsx
+++ TaskList.jsx
@@ -3,18 +3,16 @@ import TaskItem from "./TaskItem";
 function TaskList({ tasks, onDeleteTask, onToggleTask }) {
   return (
     <div
-      className="mt-4 rounded-lg p-4 bg-white transition-colors duration-300 text-black"
+      className="mt-4 rounded-lg p-5 bg-white transition-colors duration-300 text-black"
       style={{
-        maxHeight: '350px',
-        overflowY: 'auto'
+        maxHeight: "460px",
+        overflowY: "auto",
       }}
     >
       {tasks.length === 0 ? (
-        <h3 className="text-center text-black py-4 text-xl">
-          Brak zadań. Dodaj coś! 
-        </h3>
+        <h3 className="text-center text-black py-6 text-2xl">Brak zadan. Dodaj cos!</h3>
       ) : (
-        <ul className="space-y-2">
+        <ul className="space-y-3">
           {tasks.map((task) => (
             <TaskItem
               key={task.id}
@@ -27,6 +25,6 @@ function TaskList({ tasks, onDeleteTask, onToggleTask }) {
       )}
     </div>
   );
-};
+}
 
 export default TaskList;
